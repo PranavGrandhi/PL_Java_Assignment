21a49b76c5a70b031659d852c6312b9d47c1952a
PranavGrandhi <pranav.grandhi@gmail.com>
2023-12-13 01:37:41 -0500

    whole code

--- Part1.java
+++ Part1.java
@@ -1,99 +1,89 @@
 import java.util.*;
 
+public class SortedList<T extends Comparable<T>> extends ArrayList<T> implements Comparable<SortedList<T>> {
+    @Override
+    public boolean add(T element) {
+        super.add(element);
+        Collections.sort(this);
+        return true;
+    }
+
+    @Override
+    public int compareTo(SortedList<T> secondSortedList) {
+        int min_size = Math.min(this.size(), secondSortedList.size());
+        for(int i = 0; i < min_size; i++)
+        {
+            int compareBool = this.get(i).compareTo(secondSortedList.get(i));
+            if(compareBool != 0)
+                return compareBool; 
+        }
+        if(this.size() == secondSortedList.size())
+            return 0;
+        if(this.size() > secondSortedList.size())
+            return 1;
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        String mySortedList = "";
+        for(int i = 0; i < this.size(); i++)
+        {
+            mySortedList += (this.get(i).toString() + " ");
+        }
+        return (mySortedList + '\n');
+    }
+}
+public class A implements Comparable<A> {
+    Integer a;
+    A(Integer x) {
+        this.a = x;
+    }
+    public Integer getInteger() {
+        return this.a;
+    }
+    public Integer getSum() {
+        return this.a;
+    }
+    @Override
+    public int compareTo(A other) {
+        return this.getInteger().compareTo(other.getInteger());
+    }
+    @Override
+    public String toString() {
+        return (this.getInteger().toString() + " ");
+    }
+}
+
+public class B extends A {
+    Integer a;
+    Integer b;
+    B(Integer x, Integer y) {
+        super(x);
+        this.b = y;
+    }
+    public Integer getSum() {
+        return (this.a + this.b);
+    }
+    @Override
+    public int compareTo(A other) {
+        return this.getSum().compareTo(other.getSum());
+    }
+    @Override
+    public String toString() {
+        return (a.toString() + " " + b.toString() + " ");
+    }
+}
+
 public class Part1 {
-    
-    public class SortedList<T extends Comparable<T>> extends ArrayList<T> implements Comparable<SortedList<T>> {
-        @Override
-        public boolean add(T element) {
-            super.add(element);
-            Collections.sort(this);
-            return true;
-        }
-
-        @Override
-        public int compareTo(SortedList<T> secondSortedList) {
-            int min_size = Math.min(this.size(), secondSortedList.size());
-
-            for(int i = 0; i < min_size; i++)
-            {
-                int compareBool = this.get(i).compareTo(secondSortedList.get(i));
-                if(compareBool != 0)
-                    return compareBool; 
-            }
-
-            if(this.size() == secondSortedList.size())
-                return 0;
-            if(this.size() > secondSortedList.size())
-                return 1;
-            return -1;
-        }
-
-        @Override
-        public String toString() {
-            String mySortedList = "";
-
-            for(int i = 0; i < this.size(); i++)
-            {
-                mySortedList += (this.get(i).toString() + " ");
-            }
-
-            return (mySortedList + '\n');
-        }
+    static <T extends Comparable<T>> void addToSortedList(SortedList<T> L, T z) {
+        L.add(z);
     }
 
-    public class A implements Comparable<A> {
-        Integer a;
-
-        A(Integer x) {
-            this.a = x;
-        }
-
-        public Integer getInteger() {
-            return this.a;
-        }
-
-        public Integer getSum() {
-            return this.a;
-        }
-
-        @Override
-        public int compareTo(A other) {
-            return this.getInteger().compareTo(other.getInteger());
-        }
-
-        @Override
-        public String toString() {
-            return (this.getInteger().toString() + " ");
-        }
-    }
-
-    public class B extends A {
-        Integer a;
-        Integer b;
-
-        B(Integer x, Integer y) {
-            super(x);
-            this.b = y;
-        }
-
-        public Integer getSum() {
-            return (this.a + this.b);
-        }
-
-        @Override
-        public int compareTo(A other) {
-            return this.getSum().compareTo(other.getSum());
-        }
-
-        @Override
-        public String toString() {
-            return (a.toString() + " " + b.toString() + " ");
-        }
-    }
-
-    /*static void test() {
+    static void test() {
         SortedList<A> c1 = new SortedList<A>();
         SortedList<A> c2 = new SortedList<A>();
+        
         for(int i = 35; i >= 0; i-=5) {
             addToSortedList(c1, new A(i));
             addToSortedList(c2, new B(i+2,i+3));
@@ -119,9 +109,9 @@ public class Part1 {
             System.out.println("Uh Oh");
             break;
         }
-       }*/
-
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hi");
+        test();
     }
 }
